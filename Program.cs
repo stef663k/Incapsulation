bc8562fce05b97c18a6a3a96e2063d45650188f3
stef663k <stef663k@elev.techcollege.dk>
2022-08-30 09:06:33 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Incapsulation.Protected;
+
+namespace Incapsulation;
+
+
+class Incapsulation
+{
+    public static void Main(String[] args)
+    {
+        var animal = new Mammal();
+        animal.B = 1;
+    }
+}
---
+++ Protected/Animal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Incapsulation.Protected;
+
+ class Animal
+{
+    protected string A;
+    protected string B;
+}
